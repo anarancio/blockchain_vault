cf3e83d49cf5e3d3fe6a939df23de8cf9da17312
Alejandro Narancio <ale.narancio@gmail.com>
2019-07-21 19:26:37 -0300

    fundraising example: adding openzeppelin ownable and changing state variables names

--- 02_Solidity_Examples/simple_fundraising/contracts/Fundraising.sol
+++ 02_Solidity_Examples/simple_fundraising/contracts/Fundraising.sol
@@ -1,52 +1,50 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import "./SafeMath.sol";
+import './Ownable.sol';
 
 /// @title Fundraising example contract.
 /// @author Alejandro Narancio
 /// @notice This contract will allow to make a crowd fundraising using ethers, please see the readme for the rules considered in the contract.
-contract Fundraising {
+contract Fundraising is Ownable {
 
     using SafeMath for uint;
 
-    address private owner;
+    uint private _deadline;
 
-    uint private deadline;
+    uint private _minimumGoal;
 
-    uint private minimumGoal;
+    uint private _accumulatedDonations;
 
-    uint private accumulatedDonations;
-
-    mapping (address => uint) donations;
+    mapping (address => uint) _donations;
 
     /// @notice Create the contract, initilize the fundraising attributes
-    /// @param _deadline number of seconds the fundraising period will be active/open
-    /// @param _minimumGoal the minimum amount of wei it must be collected in order to allow the owner to withdraw the funding
-    constructor(uint _deadline, uint _minimumGoal) public {
-        owner = msg.sender;
+    /// @param deadline number of seconds the fundraising period will be active/open
+    /// @param minimumGoal the minimum amount of wei it must be collected in order to allow the owner to withdraw the funding
+    constructor(uint deadline, uint minimumGoal) public {
         // we will use now in order to get the block timestamp, but remember that this variable can be manipulated by the miners.
         // for our educational purpose is fine to use now
-        deadline = now + _deadline;
-        minimumGoal = _minimumGoal;
+        _deadline = now + deadline;
+        _minimumGoal = minimumGoal;
     }
 
     /// @notice Returns the total amount of donations made by a specific address.
     /// @param _donor The address we want to get the donations.
     function donationsOf(address _donor) public view returns (uint) {
-        return donations[_donor];
+        return _donations[_donor];
     }
 
     /// @notice Returns the total accumulated donations. 
     function accumulatedDonations() public view returns (uint) {
-        return accumulatedDonations;
+        return _accumulatedDonations;
     }
 
     /// @notice Receive a donation
     function donate() public payable {
-        require(now <= deadline, "The fundraising period is closed.");
+        require(now <= _deadline, "The fundraising period is closed.");
         require(msg.value > 0, "Your donation can't be 0.");
-        donations[msg.sender] = donations[msg.sender].add(msg.value);
-        accumulatedDonations = accumulatedDonations.add(msg.value);
+        _donations[msg.sender] = _donations[msg.sender].add(msg.value);
+        _accumulatedDonations = _accumulatedDonations.add(msg.value);
     }
 
 }
